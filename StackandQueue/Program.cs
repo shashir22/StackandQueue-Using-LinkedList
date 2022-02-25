7b60774f4b6f0045af94f6962ddba0071d3114c1
Shashikumar <shashirgowdas67@gmail.com>
2022-02-25 21:43:24 +0530

    Added:Queue using linked list

--- StackandQueue/Program.cs
+++ StackandQueue/Program.cs
@@ -7,14 +7,14 @@ namespace StackandQueue
         static void Main(string[] args)
         {
             Console.WriteLine("welcome to Stack and Queue Programs");
-            LinkedListStack linkedListStack = new LinkedListStack();
-            linkedListStack.Push(70);
-            linkedListStack.Push(30);
-            linkedListStack.Push(56);
-            linkedListStack.Display();
-            linkedListStack.Peek();
-            linkedListStack.Pop();
-            linkedListStack.IsEmpty();
+            LinkedListQueue linkedListQueue = new LinkedListQueue();
+            linkedListQueue.Enqueue(70);
+            linkedListQueue.Enqueue(30);
+            linkedListQueue.Enqueue(56);
+            linkedListQueue.Display();
+            //linkedListStack.Peek();
+            //linkedListStack.Pop();
+            //linkedListStack.IsEmpty();
         }
     }
 }
